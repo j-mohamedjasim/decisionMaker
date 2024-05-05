5b915cbb6870cd9d703cf11338c1fd2132cf7206
Mohamed Jasim <167366935+j-mohamedjasim@users.noreply.github.com>
2024-05-05 19:37:33 +0100

    Updated comments

--- decisionMaker.py
+++ decisionMaker.py
@@ -1,25 +1,27 @@
 import random
 
+#Random function will randomly pick one of the item below
 answer = ['Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No','Yes','No',]
 
 question = input('What do you need help with making decision? ')
-
+#Picked one by the random function will be append to the list below
 ans = []
 
-def answ():
-    for i in range(10):
+def answ():#Function to pick random decision
+    for i in range(10):#This will pick 10 random items from the answer list above
         
         a = random.choice(answer)
         ans.append(a)
 
-        #print(a)
-    c1 = ans.index('Yes')
-    c2 = ans.index('No')
+        
+    c1 = ans.index('Yes')#This will count how many YES from the ans list above
+    c2 = ans.index('No')#This will count how many NO from the ans list above
     print()
     print(question)
     print()
     print('Yes: ',c1)
     print('No:',c2)
+    print()
 
 
 answ()
